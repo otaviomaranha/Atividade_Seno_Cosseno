ed995d8c284548e838c4fcc8adfdff50aca441af
otaviomaranha <otaviomaranha.contato@gmail.com>
2025-06-19 21:07:16 -0300

    Create atvd.py

---
+++ atvd.py
@@ -0,0 +1,8 @@
+import math
+angulo = int(input('digite o angulo que deseja: '))
+seno = math.sin(math.radians(angulos))
+print('o angulo de {} tem o seno de {}.'.format(angulo, seno))
+cosseno = math.cos(math.radians(angulos))
+print('o angulo de {} tem o cosseno de {}.'.format(angulo, cosseno))
+tangente = math.tan(math.radians(angulos))
+print('o angulo de {} tem a tangente de {}.'.format(angulo, tangente))
